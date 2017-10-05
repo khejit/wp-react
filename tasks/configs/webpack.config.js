10cb8334d6e1240488010af1d63f0516ca2f3b6b
Mikołaj Śliwiński <misliwinski@ad.neuca.pl>
2017-10-05 12:15:00 +0200

    new test build 2

--- tasks/configs/webpack.config.js
+++ tasks/configs/webpack.config.js
@@ -49,6 +49,9 @@ module.exports = {
           comments: false,
           minimize: false
         }),
-        new webpack.optimize.AggressiveMergingPlugin()
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        })
     ]
 };
